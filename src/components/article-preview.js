ce8c2e18fd57299c7f8f3b8b800be821088d4c3f
Blake Ingenthron <blakeingenthron@gmail.com>
2020-03-19 14:29:34 -0500

    Test commit

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -18,7 +18,19 @@ export default ({ article }) => (
     />
     {article.tags &&
       article.tags.map(tag => (
-        <p className={styles.tag} key={tag}>
+        <p
+          style={{
+            color: '#A0A0A0',
+            textdDecoration: 'none',
+            display: 'inline-block',
+            padding: '.33333rem .5rem',
+            lineHeight: '1',
+            borderRadius: '2px',
+            border: '1px solid #A0A0A0',
+            marginRight: '.5em',
+          }}
+          key={tag}
+        >
           {tag}
         </p>
       ))}
