ccb0e683df674daade954082b502db874a461dda
Blake Ingenthron <blakeingenthron@gmail.com>
2020-03-31 15:01:07 -0500

    Update home-slide.js

--- src/components/home-slider/slides/home-slide.js
+++ src/components/home-slider/slides/home-slide.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Tilt from '../../../images/tilt_houston.jpg';
-import { animated, useSpring, config } from 'react-spring';
-import Typical from 'react-typical';
+import React from 'react'
+import Tilt from '../../../images/tilt_houston.jpg'
+import { animated, useSpring, config } from 'react-spring'
+import Typical from 'react-typical'
 
-import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from 'react-responsive'
 
 const steps = [
   '',
@@ -15,34 +15,34 @@ const steps = [
   'Social Media Marketing',
   3000,
   'Graphic Design',
-  3000
-];
+  3000,
+]
 
 const HomeSlide = () => {
   const fade = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
     delay: 1000,
-    config: config.molasses
-  });
+    config: config.molasses,
+  })
 
   const focus = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
     delay: 3000,
-    config: config.molasses
-  });
+    config: config.molasses,
+  })
 
   const type = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
     delay: 5000,
-    config: config.molasses
-  });
+    config: config.molasses,
+  })
 
   const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-device-width: 1224px)'
-  });
+    query: '(min-device-width: 1224px)',
+  })
 
   return (
     <div
@@ -52,23 +52,22 @@ const HomeSlide = () => {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         height: '100%',
-        boxShadow: '0 3000px rgba(51, 51, 51, 0.3) inset'
+        boxShadow: '0 3000px rgba(51, 51, 51, 0.3) inset',
       }}
     >
-      <div className="flex h-full  lg:items-center lg:justify-center">
-        <div className="w-full lg:w-auto inline-block text-left mt-40 lg:mt-0 px-6 lg:px-0">
+      <div className="flex h-full lg:items-center lg:justify-center">
+        <div className="inline-block w-full px-6 mt-40 text-left lg:w-auto lg:mt-0 lg:px-0">
           <animated.h1
             style={{
               ...fade,
-              fontSize: isDesktopOrLaptop ? '6rem' : '2rem'
             }}
-            className=" sm:text-5xl md:text-6xl lg:text-7xl tracking-wide text-white font-bold lg:font-extrabold"
+            className="font-bold tracking-wide text-white  sm:text-5xl md:text-6xl lg:text-7xl lg:font-extrabold"
           >
             A Creative Agency
           </animated.h1>
           <span>
             <svg
-              className="-mb-6 fill-current text-orange h-8 w-8"
+              className="w-8 h-8 -mb-6 fill-current text-orange"
               viewBox="0 0 45 44"
             >
               <path d="M0 22v22h9V9h36V0H0v22z" />
@@ -76,7 +75,7 @@ const HomeSlide = () => {
           </span>
           <animated.span
             style={focus}
-            className="pl-4 text-xl lg:text-4xl text-white font-thin italic"
+            className="pl-4 text-xl italic font-thin text-white lg:text-4xl"
           >
             Focused on{''}
           </animated.span>
@@ -92,7 +91,7 @@ const HomeSlide = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default HomeSlide;
+export default HomeSlide
