95592d409f2c682abd3d93d0a3286e5e5b8246d5
Blake Ingenthron <blakeingenthron@gmail.com>
2020-04-02 15:35:56 -0500

    Update slider.js

--- src/components/home-slider/slider.js
+++ src/components/home-slider/slider.js
@@ -1,55 +1,55 @@
-import { Link } from 'gatsby';
-import React, { useEffect, useState } from 'react';
-import { animated, config, useTransition } from 'react-spring';
-import Coobo from '../../images/coobo-logo-white.png';
-import Brand from './slides/brand-slide';
-import Home from './slides/home-slide';
-import Marketing from './slides/marketing-slide';
-import Print from './slides/print-slide';
-import Web from './slides/web-slide';
+import { Link } from 'gatsby'
+import React, { useEffect, useState } from 'react'
+import { animated, config, useTransition } from 'react-spring'
+import Coobo from '../../images/coobo-logo-white.png'
+import Brand from './slides/brand-slide'
+import Home from './slides/home-slide'
+import Marketing from './slides/marketing-slide'
+import Print from './slides/print-slide'
+import Web from './slides/web-slide'
 
 const slides = [
   {
     id: 0,
-    slide: <Home />
+    slide: <Home />,
   },
   {
     id: 1,
-    slide: <Web />
+    slide: <Web />,
   },
   {
     id: 2,
-    slide: <Print />
+    slide: <Print />,
   },
   {
     id: 3,
-    slide: <Brand />
+    slide: <Brand />,
   },
   {
     id: 4,
-    slide: <Marketing />
-  }
-];
+    slide: <Marketing />,
+  },
+]
 
 const Slider = () => {
-  const [index, set] = useState(0);
+  const [index, set] = useState(0)
   const transitions = useTransition(slides[index], item => item.id, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
-    config: config.gentle
-  });
+    config: config.gentle,
+  })
   useEffect(
-    () => void setInterval(() => set(state => (state + 1) % 5), 265000),
+    () => void setInterval(() => set(state => (state + 1) % 5), 26000),
     []
-  );
+  )
   return transitions.map(({ item, props, key }) => (
     <animated.div
       key={key}
       className="absolute inset-y-0 z-0 w-full h-full overflow-hidden bg-center"
       style={{
         ...props,
-        willChange: 'opacity'
+        willChange: 'opacity',
       }}
     >
       <Link to="/">
@@ -67,7 +67,7 @@ const Slider = () => {
         } absolute rounded-full h-4 w-4`}
         style={{
           right: '2rem',
-          top: 'calc(50% - 3rem)'
+          top: 'calc(50% - 3rem)',
         }}
         onClick={() => set(0)}
       />
@@ -77,7 +77,7 @@ const Slider = () => {
         } absolute rounded-full h-4 w-4`}
         style={{
           right: '2rem',
-          top: 'calc(50% - 1.5rem)'
+          top: 'calc(50% - 1.5rem)',
         }}
         onClick={() => set(1)}
       />
@@ -87,7 +87,7 @@ const Slider = () => {
         } absolute rounded-full h-4 w-4`}
         style={{
           right: '2rem',
-          top: '50%'
+          top: '50%',
         }}
         onClick={() => set(2)}
       />
@@ -97,7 +97,7 @@ const Slider = () => {
         } absolute rounded-full h-4 w-4`}
         style={{
           right: '2rem',
-          top: 'calc(50% + 1.5rem)'
+          top: 'calc(50% + 1.5rem)',
         }}
         onClick={() => set(3)}
       />
@@ -107,12 +107,12 @@ const Slider = () => {
         } absolute rounded-full h-4 w-4`}
         style={{
           right: '2rem',
-          top: 'calc(50% + 3rem)'
+          top: 'calc(50% + 3rem)',
         }}
         onClick={() => set(4)}
       />
     </animated.div>
-  ));
-};
+  ))
+}
 
-export default Slider;
+export default Slider
